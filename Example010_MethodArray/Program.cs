a0a036817fe8156c2b2df2cca4fe5366936cc9eb
Denis.Karev <goofy.adun@gmail.com>
2022-08-08 18:02:58 +0300

    Example010_MethodArray

---
+++ Example010_MethodArray/Program.cs
@@ -0,0 +1,16 @@
+﻿int[] arr = {1, 2, 3, 4, 5, 6, 7, 4};
+
+int n = arr.Length;
+int find = 4;
+
+int index = 0;
+
+while (index < n)
+{
+    if (arr[index]==find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;    // index +=1 // index = index +1
+}
