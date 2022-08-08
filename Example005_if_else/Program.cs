40937413c998d7cf3704cf29f3818f1ae8582aed
Denis.Karev <goofy.adun@gmail.com>
2022-08-08 17:27:08 +0300

    Example005_if_else

---
+++ Example005_if_else/Program.cs
@@ -0,0 +1,13 @@
+﻿// if_else
+Console.WriteLine ("Ввведите имя пользователя:");
+string userName = Console.ReadLine();
+
+if (userName == "Маша") //.ToLower
+{
+    Console.WriteLine ("Урал! это же Маша!");
+}
+else
+{
+    Console.Write ("Здравствуйте, ");
+    Console.WriteLine (userName);
+}
