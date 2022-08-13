d7be44d3b8c28403c56b833cdca67df6044f2a7c
Denis.Karev <goofy.adun@gmail.com>
2022-08-13 13:26:50 +0300

    Example012_methods part 2 strings chars and selectionSort

--- Example012_Methods/Program.cs
+++ Example012_Methods/Program.cs
@@ -56,11 +56,97 @@ string Method4(int count, string text)
 // string res = Method4(10, "z_");
 // Console.WriteLine(res);
 
-for (int i = 2; i <= 10; i++)
+void MultipyTable()
 {
-    for (int j = 2; j <= 10; j++)
+    for (int i = 2; i <= 10; i++)
     {
-        Console.WriteLine($"{i} x {j} = {i * j}");
+        for (int j = 2; j <= 10; j++)
+        {
+            Console.WriteLine($"{i} x {j} = {i * j}");
+        }
+        Console.WriteLine();
+    }
+}
+// MultipyTable();
+
+
+// =====Работа с текстом
+// Дан текст. В тексте нужно все пробелы заменить чёрточками,
+// маленькие буквы “к” заменить большими “К”,
+// а большие “С” маленькими “с”.
+// Ясна ли задача?
+
+string text1 = "— Я думаю, — сказал князь, улыбаясь, — что, "
+            + "ежели бы вас послали вместо нашего милого Винценгероде, "
+            + "вы бы взяли приступом согласие прусского короля. "
+            + "Вы так красноречивы. Вы дадите мне чаю?";
+
+string ReplaceChar(string text, char oldChar, char newChar)
+{
+    string resu = String.Empty;
+    int length = text.Length;
+    for (int i = 0; i < length; i++)
+    {
+        if (text[i] == oldChar) resu = resu + $"{newChar}";
+        else resu = resu + $"{text[i]}";
+    }
+    return resu;
+}
+// string newText = ReplaceChar(text1, ' ', '_');
+// Console.WriteLine (newText);
+// newText = ReplaceChar(newText, 'с', 'С');
+// Console.WriteLine (newText);
+// newText = ReplaceChar(newText, 'к', 'К');
+// Console.WriteLine (newText);
+
+
+//
+void PrintArray(int[] array)
+{
+    int count = array.Length;
+    for (int i = 0; i < count; i++)
+    {
+        Console.Write($"{array[i]} ");
+        // Console.Write(array[i] + " | ");
     }
     Console.WriteLine();
 }
+void SeletionSortMin(int[] array)
+{
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        int minPosition = i;
+
+        for (int j = i + 1; j < array.Length; j++)
+        {
+            if (array[j] < array[minPosition]) minPosition = j;
+        }
+
+        int tempopary = array[i];
+        array[i] = array[minPosition];
+        array[minPosition] = tempopary;
+    }
+}
+void SeletionSortMax(int[] array)
+{
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        int maxPosition = i;
+
+        for (int j = i + 1; j < array.Length; j++)
+        {
+            if (array[j] > array[maxPosition]) maxPosition = j;
+        }
+
+        int tempopary = array[i];
+        array[i] = array[maxPosition];
+        array[maxPosition] = tempopary;
+    }
+}
+int[] arr = { 6, 1, 8, 3, 2, 6, 4, 5, 7, 11, 0, 1, 3 };
+int[] arrMax = arr;
+PrintArray(arr);
+SeletionSortMin(arr);
+PrintArray(arr);
+SeletionSortMax(arrMax);
+PrintArray(arrMax);
